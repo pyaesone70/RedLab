6d40648de2dc68ad5aa252983e42141668214b51
RED RUBY <pyaesone7557@gmail.com>
2025-09-21 14:57:33 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const btns=document.querySelectorAll(".buttons button");
+const cards=document.querySelectorAll(".card");
+for(let i=1;i<btns.length; i++)
+{
+	btns[i].addEventListener("click", filterCards)
+}
+
+function filterCards(e)
+{
+	cards.forEach(card =>{
+		card.style.display="block";
+		const cardType=card.dataset.info;
+		const btnType=e.target.dataset.btn;
+		if(cardType !==btnType)
+			{	card.style.display="none";	}
+	})
+}
+btns[0].addEventListener("click", (e)=>{
+	cards.forEach(card =>{
+		card.style.display="block";
+	})
+})
